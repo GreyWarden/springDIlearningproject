1bf4d53bdbe94b50fb59178b87e530d3b243291f
rpavon <raulpavonfp@gmail.com>
2020-05-23 20:52:37 +0200

    First commit

---
+++ src/test/java/com/greywarden/springDIlearningproject/SpringDIlearningprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greywarden.springDIlearningproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDIlearningprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
